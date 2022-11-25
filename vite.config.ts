f7d7ee7bbdfa16511915b4e8c1ddaf6ec9999b5b
Marko Herćan <marxcartoonee@gmail.com>
2022-11-25 15:28:35 +0100

    404 again

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tools/',
-  plugins: [react(), reactScopedCssPlugin()],
+  plugins: [react({ include: ['./404.html'] }), reactScopedCssPlugin()],
 });
